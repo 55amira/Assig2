29fb2ee216bbb4454ec777df37c3064cf42a4884
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-01-04 13:07:43 +0200

    inhertence overriding ploymoephism

---
+++ Demo/Binding/PartTimeEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Binding
+{
+    internal class PartTimeEmployee : Employee
+    {
+      
+        public double HoureRate { get; set; }
+        public int NumberOfHoure { get; set; }
+
+
+        public new void Fun01()
+        {
+            Console.WriteLine("I am Employee ");
+        }
+
+        public override void Fun02()
+        {
+            Console.WriteLine($"salary = {HoureRate*NumberOfHoure}");
+        }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,125 @@
-﻿namespace Demo
+﻿using Demo.Binding;
+using Demo.Overriding;
+using System;
+
+namespace Demo
 {
     internal class Program
     {
+
+        //static void EmployeeProcess(FullTimeEmployee employee)
+        //{
+        //    employee.Fun01();
+        //    employee.Fun02();
+        //}
+
+        //static void EmployeeProcess(PartTimeEmployee employee)
+        //{
+        //    employee.Fun01();
+        //    employee.Fun02();
+        //}
+
+        
+
+        static void EmployeeProcess(Employee employee)
+        {
+            employee.Fun01();
+            employee.Fun02();
+        }
         static void Main(string[] args)
         {
+
+            //static int Sum(int X, int Y)
+            //{
+            //    return X + Y;
+            //}
+
+            //static int Sum(int X, int Y, int Z)
+            //{
+            //    return X + Y + Z;
+            //}
+
+            //static double Sum(double X, double Y)
+            //{
+            //    return X + Y;
+            //}
+
+            //static double Sum(int X, double Y)
+            //{
+            //    return X + Y;
+            //}
+
+            //static double Sum(double X, int Y)
+            //{
+            //    return X + Y;
+            //}
+            static void Main(string[] args)
+            {
+                //inheritance
+
+                // Parent parent = new Parent(1, 2);
+                // Console.WriteLine(parent);
+
+                // parent.Fun01();
+                //parent.Fun02();
+
+                // Child child=new Child(1,2,3);
+                //// Console.WriteLine(child);
+                // child.Fun01();
+                // child.Fun02();
+
+                //Sum(1, 2);
+                //Sum(1.3, 4.3);
+
+                // TypeB typeB = new TypeB() { A = 12, B = 13 };
+                //typeB.A = 12;
+                //typeB.B = 13;
+
+                // Console.WriteLine("Press any key to exit...");
+                // Console.ReadKey();
+
+                //typeB.Fun01();
+                //typeB.Fun02();
+
+                //TypeA Ref;
+                //Ref = new TypeA();
+                //Ref = new TypeB();
+
+                // Ref.A = 12;
+                //  Ref.B = 13;
+                // 
+                // Ref.Fun01();
+                // Ref.Fun02();
+                //
+
+               // Console.WriteLine("lkjnhbv");
             
+
+                FullTimeEmployee fullTime = new FullTimeEmployee()
+                {
+                    id = 1,
+                    name= "ahmed",
+                    email="ah@.com",
+                    salary=12000,
+                    address=",cmvnb"
+                };
+
+                PartTimeEmployee partTime = new PartTimeEmployee()
+                {
+                    id = 1,
+                    name = "ahmed",
+                    email = "ah@.com",
+                    HoureRate = 300,
+                    NumberOfHoure = 200,
+                    address = ",cmvnb"
+
+                };
+
+                    //8EmployeeProcess(fullTime);
+                     EmployeeProcess(partTime);
+
+            }
         }
+
     }
 }
